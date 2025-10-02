66a90d0bb281424af9944216dbb3cf31d855aa94
Algorhythmic <algorhythmicindustries@gmail.com>
2025-10-01 22:00:26 -0400

    Changed tsconfig to not compile test files, and removed dist directory entirely

--- sdk/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './createSmartAccount';
-export * from './simpleTransaction';
-export * from './complexTransaction';
-export * from './complexBufferedTransaction';
-export * from './simpleTransaction';
