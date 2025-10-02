109e496b008d8c3748e797572e09186919a32f56
Algorhythmic <algorhythmicindustries@gmail.com>
2025-10-01 22:29:59 -0400

    Configure AWS CodePipeline to build SDK from source
    - Remove sdk/dist from repository (add back to .gitignore)
    - Update frontend buildspec.yml to build SDK before frontend
    - Simplify frontend build script (remove redundant npm install)
    - SDK will now be compiled fresh in CI/CD instead of committing compiled files
    
    This eliminates the need to manually build and commit dist files while
    ensuring AWS CodePipeline has access to compiled JavaScript.

--- sdk/dist/utils/jupiterApi.d.ts
+++
@@ -1,3 +0,0 @@
-import { JupiterQuote, JupiterSwapResponse } from '../types/jupiter';
-export declare function getSwapQuote(inputMint: string, outputMint: string, amount: number, slippage: number, platformFeeBps?: number): Promise<JupiterQuote>;
-export declare function getSwapTransaction(quote: JupiterQuote, userPublicKey: string, wrapAndUnwrapSol?: boolean, dynamicComputeUnitLimit?: boolean, prioritizationFeeLamports?: string | number): Promise<JupiterSwapResponse>;
