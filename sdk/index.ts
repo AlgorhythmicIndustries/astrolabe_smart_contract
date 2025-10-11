8c29e29f88acbf7314225969a9f6c8f0cac34268
Algorhythmic <algorhythmicindustries@gmail.com>
2025-10-11 10:08:03 -0400

    Added high level sdk function for adding new passkey authority

--- sdk/index.ts
+++ sdk/index.ts
@@ -2,4 +2,5 @@ export * from './createSmartAccount';
 export * from './simpleTransaction';
 export * from './complexTransaction';
 export * from './complexBufferedTransaction';
+export * from './addPasskeyAuthority';
 export * from './utils/index';
